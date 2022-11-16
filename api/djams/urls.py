71601c2b197cf15932b1f8189730524f7961f868
Joshtdale <joshua.dale@student.awesomeinc.org>
2022-11-16 22:03:43 +0000

    create many to many

--- api/djams/urls.py
+++ api/djams/urls.py
@@ -5,6 +5,7 @@ from rest_framework import routers
 router = routers.SimpleRouter()
 router.register(r'genre', GenreViewSet)
 router.register(r'song', SongViewSet)
+router.register(r'artist', ArtistViewSet)
 
 urlpatterns = [
     # path('song/', SongAPIView.as_view()),
