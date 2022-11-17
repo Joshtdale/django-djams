6c867bfa2a0fe5f2f1b9f86d105fbd6f1650c305
Joshtdale <joshua.dale@student.awesomeinc.org>
2022-11-17 21:03:56 +0000

    stuff2

--- api/djams/fields.py
+++ api/djams/fields.py
@@ -1,8 +1,19 @@
-# from rest_framework import serializers
-# from .models import *
+from rest_framework import serializers
+from .models import *
 
-# class SongField(serializers.RelatedField):
-#     def to_internal_value(self, data):
-
-#     def to_representation(self, value):
-#         return instance.name
+class GenreField(serializers.RelatedField):
+    def to_internal_value(self, data):
+        obj, created = Genre.objects.get_or_create(**data)
+        return obj
+    def to_representation(self, value):
+        return {
+            "name": value.name
+        }
+class ArtistField(serializers.RelatedField):
+    def to_internal_value(self, data):
+        obj, created = Artist.objects.get_or_create(**data)
+        return obj
+    def to_representation(self, value):
+        return {
+            "name": value.name
+        }
